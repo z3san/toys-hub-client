278e59c2eee9172179a14f335781a0de763a2aa3
z3san <ahtisamhaque@gmail.com>
2023-05-17 21:31:15 +0600

    firebase authentication sytem added and folder structure is created

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,12 @@
+import App from "../App";
+import { createBrowserRouter } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App></App>
+    }
+])
+
+export default router
